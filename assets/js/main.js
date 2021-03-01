72e4600c5e6624fa82d63b3f81e68acc36c87487
Claudio <claudiocarchietti@yahoo.it>
2021-03-01 14:14:42 +0100

    inizio esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var etapasseggero = prompt("Inserire l'età del passeggero");
+
+var etapasseggero = isNAN(etapasseggero);
+
+
+
+console.log("età passeggero = " + etapasseggero);
+
+
+
+var percorso = prompt("Quanti kilometri vuoi percorrere");
+console.log("Lunghezza percorso = " + percorso + "km");
